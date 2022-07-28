66f1961ffc367d9830b73eb64e80d484d35c5ec3
sam <sam990126@gmail.com>
2022-07-28 19:32:00 +0800

    新增indicator bar跟左右滑動的按鈕

--- myModule.js
+++ myModule.js
@@ -101,7 +101,8 @@ class mainPage{
 		
 		//幫其他被滑動的物品綁定滑動事件
 		this.showOtherElement(event.data.index);
-			
+		this.createSlidingIndicator(event.data.index);
+		this.createSlideArrow();
 	}
 	elementChangeSize=(el,size)=>{
 		//el param是 class或id的name
@@ -202,9 +203,10 @@ class mainPage{
 	
 	slide = (direction) => {
 			if(direction=="next"){
-				if(this.currentSlidePage-1<0)return;
 				//往左滑
+				if(this.currentSlidePage-1<0)return;
 				this.currentSlidePage-=1;
+				this.changeIndicatorCircleColor(this.currentSlidePage);
 				for(let i=0;i<this.length;i++){
 					let nid="#_expandChild"+i;
 					$(nid).animate({
@@ -215,6 +217,7 @@ class mainPage{
 				//往右滑
 				if(this.currentSlidePage+1>=this.length)return;
 				this.currentSlidePage+=1;
+				this.changeIndicatorCircleColor(this.currentSlidePage);
 				for(let i=0;i<this.length;i++){
 					let nid="#_expandChild"+i;
 					$(nid).animate({
@@ -223,5 +226,133 @@ class mainPage{
 				}
 			}
 	};
+	changeIndicatorCircleColor=(idx)=>{
+		//idx為當前被選擇的inedx,將idx的color轉成白色(被選擇),其他則轉為灰色(沒被選擇
+		for(let i=0;i<this.length;i++){
+				if(idx!=i){
+					$("#_indicatorCircle"+i).css({
+						"opacity":0.2
+					});
+				}else{
+					$("#_indicatorCircle"+i).css({
+						"opacity":0.7
+					})
+				}
+		}
+	}
+	createSlidingIndicator=(initIdx)=>{
+		let el=document.createElement("div");
+		let barWidth=this.originWidth*0.2;
+		let barHeight=30;
+		let marginBottom=this.originHeight*0.1;
+
+		//預設寬度為container的10%
+		$(el).css({
+			"width":barWidth+"px",
+			"height":barHeight+"px",
+			"position":"absolute",
+			"bottom":marginBottom+"px",
+			"left":(this.originWidth/2)-(barWidth/2)+"px"
+		});
+		this.container.append(el);
+		
+
+		
+		// let circleWidth=(barWidth-(marginBase*this.length))/this.length;
+		// let circleHeight=barHeight*0.9;
+		let circleWidth=10;
+		let circleHeight=10;
+		let horizontalMargin=(barWidth-(circleWidth*this.length))/(this.length+1);
+		let verticalMargin=barHeight/2-(circleHeight/2);
+		for(let i=0;i<this.length;i++){
+				let circle=document.createElement("span");
+				$(circle).css({
+					"width":circleWidth+"px",
+					"height":circleHeight+"px",
+					"border-radius":"50%",
+					"background-color":"white",
+					"display":"inline-block",
+					"margin-left":horizontalMargin+"px",
+					"margin-top":verticalMargin+"px",
+					"opacity":initIdx==i?0.7:0.2
+				});
+				$(circle).attr("id","_indicatorCircle"+i);
+				$(el).append(circle);
+		}
+	}
+	
+	createSlideArrow=()=>{
+			//right arrow
+			let arrowLength=18;
+			let borderWidth=3;
+			let circleR=30;
+			let offset=(circleR/2)-(arrowLength/2)-(arrowLength/6);
+			let outCircleRight=document.createElement("div");
+			let littleMarginOnLeftRight=3;
+			$(outCircleRight).css({
+				"width":circleR+"px",
+				"height":circleR+"px",
+				"border-radius":"50%",
+				"background-color":"white",
+				"position":"absolute",
+				"top":this.originHeight/2-(circleR/2)+"px",
+				"right":0+"px",
+				"transform": "rotate(-45deg)",
+				"transform-origin":"center center",
+				"opacity":0.7
+			});
+			this.container.append(outCircleRight);
+			let el=document.createElement("i");
+			$(el).css({
+					"border": "solid rgb(194,194,195)",
+					"border-right-width":borderWidth+"px",
+					"border-bottom-width":borderWidth+"px",
+					"border-top-width":0,
+					"border-left-width":0,
+					"display": "inline-block",
+					"padding": "0px",
+					"width":(arrowLength-borderWidth)+"px",
+					"height":(arrowLength-borderWidth)+"px",
+					"position":"absolute",
+					"top":offset+"px",
+					"left":offset+"px",
+					"opacity":1,
+			});
+			$(outCircleRight).append(el);
+			$(outCircleRight).click(()=>{this.slide("prev")});
+			
+			let outCircleLeft=document.createElement("div");
+			$(outCircleLeft).css({
+				"width":circleR+"px",
+				"height":circleR+"px",
+				"border-radius":"50%",
+				"background-color":"white",
+				"position":"absolute",
+				"top":this.originHeight/2-(circleR/2)+"px",
+				"left":littleMarginOnLeftRight+"px",
+				"transform": "rotate(135deg)",
+				"transform-origin":"center center",
+				"opacity":0.7
+			});
+			this.container.append(outCircleLeft);
+			let el2=document.createElement("i");
+			$(el2).css({
+					"border": "solid rgb(194,194,195)",
+					"border-right-width":borderWidth+"px",
+					"border-bottom-width":borderWidth+"px",
+					"border-top-width":0,
+					"border-left-width":0,
+					"display": "inline-block",
+					"padding": "0px",
+					"width":(arrowLength-borderWidth)+"px",
+					"height":(arrowLength-borderWidth)+"px",
+					"position":"absolute",
+					"top":offset+"px",
+					"left":offset+"px",
+					"opacity":1
+			});
+			$(outCircleLeft).append(el2);
+			$(outCircleLeft).click(()=>{this.slide("next")});
+	}
 	
 }
